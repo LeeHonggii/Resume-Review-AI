1dd39b9f7794ae2d76297a53f2f2087b716c2989
Eddie <birth0508@gmail.com>
2024-05-07 10:13:05 +0900

    Chore Fix root templates

--- App/main.py
+++ App/main.py
@@ -1,4 +1,13 @@
-from fastapi import FastAPI, Request, Form, HTTPException, Depends, UploadFile, File, Response
+from fastapi import (
+    FastAPI,
+    Request,
+    Form,
+    HTTPException,
+    Depends,
+    UploadFile,
+    File,
+    Response,
+)
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 import hashlib
@@ -9,13 +18,13 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
-templates = Jinja2Templates(directory="App/templates")
+templates = Jinja2Templates(directory="templates")
 
 
 users = {
     "john_doe": {
         "username": "john_doe",
-        "password": hashlib.sha256("password123".encode()).hexdigest()
+        "password": hashlib.sha256("password123".encode()).hexdigest(),
     }
 }
 
@@ -33,18 +42,18 @@ async def login_form(request: Request):
 @app.post("/login")
 async def login(request: Request, username: str = Form(...), password: str = Form(...)):
     logger.debug(
-        f"Received login request with username: {username}, password: {password}")
+        f"Received login request with username: {username}, password: {password}"
+    )
 
     user = users.get(username)
-    if user and user['password'] == hashlib.sha256(password.encode()).hexdigest():
+    if user and user["password"] == hashlib.sha256(password.encode()).hexdigest():
         logger.debug("Login successful: User authenticated")
         response = RedirectResponse(url=f"/{username}", status_code=302)
         response.set_cookie(key="username", value=username, httponly=True)
         return response
     else:
         logger.error("Login failed: Invalid username or password")
-        raise HTTPException(
-            status_code=401, detail="Invalid username or password")
+        raise HTTPException(status_code=401, detail="Invalid username or password")
 
 
 @app.post("/logout")
@@ -64,10 +73,9 @@ async def user_page(request: Request, username: str):
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
 
-    return templates.TemplateResponse("user_page.html", {
-        "request": request,
-        "user": {"username": username}
-    })
+    return templates.TemplateResponse(
+        "user_page.html", {"request": request, "user": {"username": username}}
+    )
 
 
 @app.route("/upload", methods=["POST"])
@@ -81,7 +89,7 @@ async def result():
     pass
 
 
-@ app.route("/classification", methods=["POST"])
+@app.route("/classification", methods=["POST"])
 async def classification():
     pass
 
@@ -93,6 +101,4 @@ async def generation():
 
 if __name__ == "__main__":
 
-    uvicorn.run(
-        "main:app", host="127.0.0.1", port=8000, log_level="debug", reload=True
-    )
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="debug", reload=True)
