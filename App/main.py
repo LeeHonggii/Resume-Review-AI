0161f735e41eb0e294a5ccde57227a36ecec5ec1
LeeHonggii <141085834+LeeHonggii@users.noreply.github.com>
2024-05-20 15:16:00 +0900

    merge

--- App/main.py
+++ App/main.py
@@ -19,6 +19,155 @@ import cohere
 import pytz
 from openai import OpenAI
 
+# classification model setup
+import numpy as np
+from classification import Classfication_model
+from company_info import  Company_info
+from vectorDB import VectorDB
+import os
+
+
+classifier = Classfication_model()
+company_info = Company_info()
+vector_db = VectorDB()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+COHERE_API_KEY = os.getenv("COHERE_API_KEY")
+
+if not OPENAI_API_KEY:
+    raise ValueError("OPENAI_API_KEY environment variable is not set.")
+if not COHERE_API_KEY:
+    raise ValueError("COHERE_API_KEY environment variable is not set.")
+
+openai_client = OpenAI(api_key = OPENAI_API_KEY)
+co = cohere.Client(api_key=COHERE_API_KEY)
+API_URL = os.getenv("API_URL")
+
+async def gpt_generation(generate_target:str, job_title: str, text: str, comp_info : str) -> str:
+    if comp_info != "":
+        comp_prompt = f"지원하려는 회사의 인재상은 {comp_info}. 너무 직접적인 표현은 좋지 않아."
+
+    prompt = (
+        f"1.자기소개서를 작성하려고 해. 지원하는 직무 정보는 {job_title}.{comp_prompt}\n"
+        f"2. {generate_target}에 해당하는 내용을 5문장 이내로 제안해줘.\n"
+        f"3. 이건 내가 적은 자기소개서야 {text}"
+        f"4. 빈줄없이 출력해줘\n"
+        f"5. 다른 설명 없이 너의 답변을 그대로 자기소개서에 복사 붙여넣기 할 수 있도록, 자기 소개서 본문만 출력해줘."
+    )
+    try:
+        chat_completion = openai_client.chat.completions.create(
+            messages=[{"role": "user", "content": prompt}], model="gpt-4o"
+        )
+        return chat_completion.choices[0].message.content
+    except Exception as e:
+        logger.error(f"Error processing GPT response: {str(e)}")
+        return f"서버 오류 발생: {str(e)}"
+
+def chat2(prompt: str, text: str):
+    response = co.chat(
+        chat_history=[
+            {"role": "USER", "message": prompt},
+        ],
+        message=text,
+        connectors=[{"id": "web-search"}],
+    )
+    return response
+    
+async def cohere_generation(generate_target: str, job_title: str, text: str, comp_info: str) -> str:
+    prompt = (
+        f"자소서 내용 분석 (직무: {job_title}): {text}\n"
+        "1. 해당 직무에 필요한 경험, 지원 동기 및 포부, 강점, 단점이 얼마나 잘 매치되는지 분석해줘.\n"
+        "2. 누락된 요소나 추가할 내용이 있는지 제안해줘.\n"
+        "3. 빈줄없이 출력해줘\n"
+        "4. 너의 답변을 그대로 자기소개서에 복사 붙여넣기 할 수 있도록, 자기 소개서 본문만 출력해줘.\n"
+        "5. 상단의 모든 내용을 반복해서 출력하지마"
+    )
+
+    try:
+        response = chat2(prompt, text)
+        return response.text  # response 객체의 text 속성을 반환
+    except Exception as e:
+        logger.error(f"Cohere 응답 처리 중 오류 발생: {str(e)}")
+        return f"서버 오류 발생: {str(e)}"
+
+
+async def llm_generation(text: str) -> str:
+    prompt = "너는 자기소개서를 감별해주는 AI이고 성공 경험, 지원 동기 및 포부, 강점, 단점이 모두 포함되었는지 확인해서 누락된 부분이 있으면 말해주고 단점이 포함된 문장은 수정을 요청할게"
+
+    try:
+        responses = []
+
+        # 서버에 보낼 데이터
+        data = {"prompt": prompt, "text": text}
+
+        # 환경 변수에서 API URL을 가져옴
+        base_url = os.getenv("API_URL")
+        if not base_url:
+            raise ValueError("API_URL environment variable is not set.")
+
+        # URL 끝에 /process 경로를 추가
+        api_url = f"{base_url}/process"
+
+        # 서버에 POST 요청 보내기
+        response = requests.post(api_url, json=data)
+
+        if response.status_code == 200:
+            try:
+                response_data = response.json()
+                logger.debug(f"Response data: {response_data}")
+
+                # 응답 데이터에 "result" 키가 있는지 확인하고 없으면 "응답 없음" 반환
+                reply = response_data.get("result", "응답 없음")
+                responses.append(reply)
+            except requests.exceptions.JSONDecodeError:
+                logger.error("Response content is not in JSON format")
+                logger.error(f"Response text: {response.text}")
+                responses.append("응답 없음")
+        else:
+            logger.error(f"서버 오류 발생: {response.status_code}")
+            logger.error(f"Response text: {response.text}")
+            responses.append(f"서버 오류 발생: {response.status_code}")
+
+        return responses[0] if responses else "응답 없음"
+    except Exception as e:
+        logger.error(f"LLM 응답 처리 중 오류 발생: {str(e)}")
+        return f"서버 오류 발생: {str(e)}"
+
+def classify_text(text):
+    class_count = [0, 0, 0]
+    outClass = []
+    negative_result = ""
+    generate_target = ""
+
+    predict, outList = classifier.classify(text)
+    np.set_printoptions(suppress=True, precision=2)
+
+    for i in range(len(predict)):
+        c = np.argmax(predict[i])
+        class_count[c] += 1
+        outClass.append(c)
+        print(predict[i], classifier.class_name[np.argmax(predict[i])], outList[i])
+
+    print("class_count:", class_count)
+    print(outClass)
+    print(outList)
+
+    if class_count[0] != 0:  # negative sentence
+        negative_result = f"Negative Sentence Count: {class_count[0]}"
+    else:
+        negative_result = "None"
+
+    if class_count[1] != 0:
+        generate_target = '성공 경험'
+        print("성공 경험 missing.")
+    if class_count[2] != 0:
+        generate_target += '입사 동기 및 포부'
+        print("입사 동기 및 포부 missing.")
+        print("generate_target:", generate_target)
+
+    return generate_target, negative_result, outClass, outList
+
+
 app = FastAPI()
 
 # Database setup
@@ -29,12 +178,6 @@ SessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine, class_=AsyncSession
 )
 
-# OpenAI client setup
-
-
-def create_openai_client():
-    api_key = ""
-    return OpenAI(api_key=api_key)
 
 
 app.add_middleware(
@@ -313,102 +456,47 @@ async def chat_analysis(request: Request):
     job_title = data.get("job_title")
     text = data.get("text")
 
-    gpt_response = await get_gpt_responses(job_title, text) or "실패"
-    cohere_response = await get_cohere_responses(job_title, text) or "실패"
-    model_response = await get_model_responses(job_title, text) or "실패"
+    # classification input text
+    generate_target, negative_result, outClass, outList = classify_text(text)
 
-    analysis_results = [
-        {"gpt_response": gpt_response},
-        {"cohere_response": cohere_response},
-        {"model_response": model_response},
-    ]
+    # Get company details
+    comp_name, comp_info = company_info.get_company_info(job_title)
+
+    # Generate VDB prompt
+    vdb_prompt = vector_db.vdb_prompt(generate_target, job_title, comp_name, comp_info)
+    vdb_result = vector_db.query(vdb_prompt)
+    if len(vdb_result) > 0:
+        vdb_reponse = vdb_result[0]
+    else:
+        vdb_reponse = "NULL"
+    print(f"({vdb_prompt}) -> {vdb_reponse}")
+
+    # Generate responses from different models
+    gpt_response = await gpt_generation(generate_target, job_title, text, comp_info) or "실패"
+    vector_db.add(vdb_prompt, gpt_response, True)
+    # if gpt_response is valid:
+    #     vector_db.add(vdb_prompt, gpt_response)
+
+    # cohere_response = await cohere_generation(generate_target, job_title, text, comp_info) or "실패"
+    # if cohere_response is valid:
+    #     vector_db.add(vdb_prompt, cohere_response)
+    cohere_response = "NULL"   #temporally
+
+    # llm_response = await llm_generation(text) or "실패"
+    llm_response = "NULL"   #temporally
+    # if llm_response is valid:
+    #     vector_db.add(vdb_prompt, llm_response)
+
+    # Add vdb_prompt to the response
+    analysis_results = {
+        "vdb_reponse": vdb_reponse,
+        "gpt_response": gpt_response,
+        "cohere_response": cohere_response,
+        "llm_response": llm_response,
+        "negative_result": negative_result
+    }
 
     return JSONResponse(content=analysis_results)
-
-
-async def get_gpt_responses(job_title: str, text: str) -> str:
-    prompt = (
-        f"자소서 내용 분석 (직무: {job_title}): {text}\n"
-        "1. 해당 직무에 필요한 경험, 지원 동기 및 포부, 강점, 단점이 얼마나 잘 매치되는지 분석해줘.\n"
-        "2. 누락된 요소나 추가할 내용이 있는지 제안해줘.\n"
-        "3. 빈줄없이 출력해줘\n"
-        "4. 너의 답변을 그대로 자기소개서에 복사 붙여넣기 할 수 있도록, 자기 소개서 본문만 출력해줘.\n"
-        "5. 상단의 모든 내용을 반복해서 출력하지마"
-    )
-
-    try:
-        # Correctly formatted messages list
-        messages = [{"role": "user", "content": prompt}]
-
-        # Assuming you have set up the client for the OpenAI API
-        chat_completion = client.chat.completions.create(
-            messages=messages, model="gpt-4o"
-        )
-        return chat_completion.choices[0].message.content
-    except Exception as e:
-        logger.error(f"Error processing GPT response: {str(e)}")
-        return f"서버 오류 발생: {str(e)}"
-
-
-co = cohere.Client(api_key="")
-def chat2(prompt1, text):
-    response = co.chat(
-        chat_history=[
-            {"role": "USER", "message": prompt1},
-        ],
-        message=text,
-        connectors=[{"id": "web-search"}],
-    )
-    return response
-
-
-async def get_cohere_responses(job_title: str, text: str) -> str:
-    prompt = (
-        f"자소서 내용 분석 (직무: {job_title}): {text}\n"
-        "1. 해당 직무에 필요한 경험, 지원 동기 및 포부, 강점, 단점이 얼마나 잘 매치되는지 분석해줘.\n"
-        "2. 누락된 요소나 추가할 내용이 있는지 제안해줘.\n"
-        "3. 빈줄없이 출력해줘\n"
-        "4. 너의 답변을 그대로 자기소개서에 복사 붙여넣기 할 수 있도록, 자기 소개서 본문만 출력해줘.\n"
-        "5. 상단의 모든 내용을 반복해서 출력하지마"
-    )
-
-    try:
-        response = chat2(prompt, text)
-        return response.text  # response 객체의 text 속성을 반환
-    except Exception as e:
-        logger.error(f"Cohere 응답 처리 중 오류 발생: {str(e)}")
-        return f"서버 오류 발생: {str(e)}"
-
-
-# Colab에서 실행된 서버의 ngrok 공개 URL
-public_url = (
-    "https://e668-34-66-84-102.ngrok-free.app"  # Colab에서 출력된 public_url로 대체
-)
-url = f"{public_url}/process"
-
-
-async def get_model_responses(job_title: str, text: str) -> str:
-    prompt = "너는 자기소개서를 감별해주는 AI이고 성공 경험, 지원 동기 및 포부, 강점, 단점이 모두 포함되었는지 확인해서 누락된 부분이 있으면 말해주고 단점이 포함된 문장은 수정을 요청할게"
-
-    try:
-        responses = []
-
-        # 서버에 보낼 데이터
-        data = {"prompt": prompt, "text": text}
-
-        # 서버에 POST 요청 보내기
-        response = requests.post(url, json=data)
-
-        if response.status_code == 200:
-            responses.append(response.json().get("reply", "응답 없음"))
-        else:
-            logger.error(f"서버 오류 발생: {response.status_code}")
-            responses.append(f"서버 오류 발생: {response.status_code}")
-
-        return responses[0] if responses else "응답 없음"
-    except Exception as e:
-        logger.error(f"GPT 응답 처리 중 오류 발생: {str(e)}")
-        return f"서버 오류 발생: {str(e)}"
 
 
 if __name__ == "__main__":
