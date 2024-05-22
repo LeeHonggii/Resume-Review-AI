0df3953eb8be8882fb3f521374dafc93e20a8022
Eddie <birth0508@gmail.com>
2024-05-22 10:05:08 +0900

    Chore Key name change

--- App/main.py
+++ App/main.py
@@ -196,7 +196,15 @@ def classify_text(text, job_title):
         # print("입사 동기 및 포부 missing.")
         print("generate_target:", generate_target)
     comp_name, comp_info = company_info.get_company_info(job_title)
-    return generate_target, negative_result, outClass, outList, outText, comp_name, comp_info
+    return (
+        generate_target,
+        negative_result,
+        outClass,
+        outList,
+        outText,
+        comp_name,
+        comp_info,
+    )
 
 
 # LSTM으로 작동하는법
@@ -537,9 +545,15 @@ async def classify_text_api(data: dict):
     job_title = data.get("job_title")
     print(job_title)
 
-    generate_target, negative_result, outClass, outList, outText, comp_name, comp_info = (
-        classify_text(text, job_title)
-    )
+    (
+        generate_target,
+        negative_result,
+        outClass,
+        outList,
+        outText,
+        comp_name,
+        comp_info,
+    ) = classify_text(text, job_title)
 
     print(comp_info)
 
@@ -632,7 +646,9 @@ async def generate_vector_db_api(data: dict):
     comp_name = data.get("comp_name")
     comp_info = data.get("comp_info")
 
-    vdb_prompt = await vector_db.vdb_prompt(generate_target, job_title, comp_name, comp_info)
+    vdb_prompt = await vector_db.vdb_prompt(
+        generate_target, job_title, comp_name, comp_info
+    )
     vdb_result = await vector_db.query(vdb_prompt)
     if len(vdb_result) > 0:
         vdb_reponse = vdb_result[0]
